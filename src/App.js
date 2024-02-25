193bd8da8c0177d0babed895056f69f545c9861a
Ben Holmes <112626017+holmesbenjamin@users.noreply.github.com>
2024-02-25 14:39:40 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,29 +1,32 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
+import './App.css'; 
 
 function App() {
-  const [data, setData] = useState([{}])
+  const [data, setData] = useState([{}]);
 
   useEffect(() => {
-    fetch('/test').then(
-      (res) => res.json()
-    ).then(
-      data => {
-        setData(data)
-        console.log(data)
-      }
-    )
-  }, [])
+    fetch('/test')
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data);
+        console.log(data);
+      });
+  }, []);
+
   return (
-    <div>
-        {data.test ? ( 
+    <div className="app-container">
+      <h1 className="app-title">Zesty Fruits</h1> 
+      <div className="data-display">
+        {data.test ? (
           data.test.map((test, i) => (
-            <p key={i}>{test}</p>
+            <p key={i} className="data-item">{test}</p>
           ))
         ) : (
           <p>Loading...</p>
         )}
+      </div>
     </div>
   );
 }
 
-export default App
+export default App;
