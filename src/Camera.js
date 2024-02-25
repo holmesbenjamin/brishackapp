15bc211712dbaf68120482bf609200278af5b905
aiden-d <aidendawes@gmail.com>
2024-02-25 20:26:27 +0000

    Screenshot working

--- src/Camera.js
+++ src/Camera.js
@@ -1,13 +1,39 @@
-import React, {useState, useEffect} from 'react';
-import Webcam from 'react-webcam';
+import React, { useState, useEffect, useCallback, useRef } from "react";
+import Webcam from "react-webcam";
 // import './App.css';
-function Camera () {
-    
-    return (
-        // <div >
-            <Webcam className='cam' mirrored={true}    />
-        // </div>
-    );
+function Camera() {
+  const [img, setImg] = useState(null);
+  const webcamRef = useRef(null);
+  const capture = useCallback(() => {
+    const imageSrc = webcamRef.current.getScreenshot();
+    setImg(imageSrc);
+  }, [webcamRef, setImg]);
+  return (
+    <>
+      {img === null ? (
+        <>
+          <Webcam
+            className="cam"
+            mirrored={true}
+            screenshotFormat="image/jpeg"
+            ref={webcamRef}
+          />
+          <button onClick={capture}>Capture</button>
+        </>
+      ) : (
+        <>
+          <img src={img} className="img" alt="captured" />
+          <button
+            onClick={() => {
+              setImg(null);
+            }}
+          >
+            Recapture
+          </button>
+        </>
+      )}
+    </>
+  );
 }
 
-export default Camera
+export default Camera;
