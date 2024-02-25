53de8bba76508a2e185ff65b061ecc3a77cee3a4
Tjbarter <wu22796@bristol.ac.uk>
2024-02-25 23:14:26 +0000

    Init

--- src/Blurb.js
+++ src/Blurb.js
@@ -1,28 +1,22 @@
 import React, {useState, useEffect} from 'react';
 
 
-function Blurb() { 
+function Blurb(props) { 
 
   const [data, setData] = useState([{}]);
 
-  useEffect(() => {
-    fetch('/test')
-      .then((res) => res.json())
-      .then((data) => {
-        setData(data);
-        console.log(data);
-      });
-  }, []);
+  // useEffect(() => {
+  //   fetch('/test')
+  //     .then((res) => res.json())
+  //     .then((data) => {
+  //       setData(data);
+  //       console.log(data);
+  //     });
+  // }, []);
 
   return (
         <div className='blurb'>
-            {data.test ? (
-          data.test.map((test, i) => (
-            <p key={i} className="data-item">{test}</p>
-          ))
-        ) : (
-          <p>Loading...</p>
-        )}
+          <p>{props.text}</p>
         </div>
       ); 
 
