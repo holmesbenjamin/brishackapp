99290e68b905485a45b81ff949fb5ca1bc5bb731
aiden-d <aidendawes@gmail.com>
2024-02-25 18:39:09 +0000

    Logo 2

--- src/Logo.js
+++ src/Logo.js
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
-import logo from './logo.png'; // Import your felix.png image
+import logo from './logo2.png'; // Import your felix.png image
 
 function Logo() {
-    return <img src={logo} />;
+    return <img src={logo} className='logo' />;
 }
 
 export default Logo
