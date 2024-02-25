cfa1e088517fef8f81c0ce7f7c6821c31c328db2
Tjbarter <wu22796@bristol.ac.uk>
2024-02-25 20:58:01 +0000

    Button

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import './App.css';
 import Camera from './Camera';
 import Blurb from './Blurb';
 import Logo from './Logo';
+import Button from './Button';
+
+
 function App() {
   
 
@@ -13,6 +16,8 @@ function App() {
     <Logo/>
       {/* <h1 className="app-title">FruitCam</h1>  */}
       <Camera />
+
+      <Button />
 
       <Blurb />
      
