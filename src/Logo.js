c6980a0a488ea408de8fa720822e6b560cf771a7
FLX-MRSH <felixthecat8@btinternet.com>
2024-02-25 18:23:35 +0000

    Logo

---
+++ src/Logo.js
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import logo from './logo.png'; // Import your felix.png image
+
+function Logo() {
+    return <img src={logo} />;
+}
+
+export default Logo
